c1d27da0a5bde247e212e9bae3ab19d962bc00d2
JISHAN GARGACHARYA <34843832+jishangarg@users.noreply.github.com>
2018-10-14 17:18:16 +0530

    15 coming

--- Appy_and_Balloons.java
+++ Appy_and_Balloons.java
@@ -4,7 +4,7 @@ public class Main{
 	{
 		Scanner in=new Scanner(System.in);
     	  	int n=in.nextInt();
-    	  	int m=in.nextInt();
+    	  	long m=in.nextInt();
     	  	int[] ballons=new int[n];
     	  	int[] Candies=new int[n];
     		int[] Mulcan=new int[n];
@@ -25,20 +25,24 @@ public class Main{
 		int start=MulcanSorted[0];
 		int end=MulcanSorted[n-1];
 		int result=0;
-		while(end>=start)
+		while(end>start)
 		{
+            
 			int toCheck=(start+end)/2;
+            
 			int flag=0;
-			int m2=0;
+			long m2=0;
 			for(int i=0;i<n;i++)
 			{
 				if(Mulcan[i]>toCheck)
 				{
-					m2+=Math.ceil((Mulcan[i]-toCheck)/Candies[i]);
+					m2+=Math.ceil((Mulcan[i]-toCheck+0.00)/Candies[i]);
+                   
 				}
 				if(m2>m)
 				{
 					flag=1;
+                   
 					break;
 				}
 				
@@ -49,7 +53,9 @@ public class Main{
 			    	result=toCheck;
 				end=toCheck;
 			}
+            
 		}
+        
 		System.out.println(result);
     		
     		
