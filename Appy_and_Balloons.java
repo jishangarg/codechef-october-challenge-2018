0be2e5c023d63617c147e921954ed3864e4d08f5
JISHAN GARGACHARYA <34843832+jishangarg@users.noreply.github.com>
2018-10-14 15:59:23 +0530

    Update Appy_and_Balloons.java

--- Appy_and_Balloons.java
+++ Appy_and_Balloons.java
@@ -17,10 +17,40 @@ public class Main{
     		for(int i=0;i<n;i++)
     		{
       			Candies[i]=in.nextInt();	  
-      			Mulcan[i]=Candies[i]*ballons[i];	    
+      			Mulcan[i]=Candies[i]*ballons[i];
+			MulcanSorted[i]=Candies[i]*ballons[i];
     		}		
-    
+    		
+		Arrays.sort(MulcanSorted);
+		int start=0;
+		int end=n-1;
+		
+		while(end>=start)
+		{
+			int toCheck=(MulcanSorted[end]+MulcanSorted[start])/2;
+			int flag=0;
+			int m2=0;
+			for(int i=0;i<n;i++)
+			{
+				if(Mulcan[i]>toCheck)
+				{
+					m2+=ceil(Mulcan[i]-toCheck)/5;
+				}
+				if(m2>m)
+				{
+					flag=1;
+					break;
+				}
+				
+			}
+			if(flag==1)
+				start=toCheck+1;
+			else
+				end=toCheck-1;
+		}
     		
     		
 	}
 }
+
+
