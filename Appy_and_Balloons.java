73f0f49600d0b95fd7906c7ee408a5a888fac2d4
JISHAN GARGACHARYA <34843832+jishangarg@users.noreply.github.com>
2018-10-14 18:03:59 +0530

    Update Appy_and_Balloons.java

--- Appy_and_Balloons.java
+++ Appy_and_Balloons.java
@@ -4,19 +4,19 @@ public class Main{
 	{
 		Scanner in=new Scanner(System.in);
     	  	int n=in.nextInt();
-    	  	long m=in.nextInt();
-    	  	long[] ballons=new int[n];
-    	  	long[] Candies=new int[n];
+    	  	long m=in.nextLong();
+    	  	long[] ballons=new long[n];
+    	  	long[] Candies=new long[n];
     		long[] Mulcan=new long[n];
     		long[] MulcanSorted=new long[n];		
     		for(int i=0;i<n;i++)
     		{
-      			ballons[i]=in.nextInt();	    
+      			ballons[i]=in.nextLong();	    
     		}
 		
     		for(int i=0;i<n;i++)
     		{
-      			Candies[i]=in.nextInt();	  
+      			Candies[i]=in.nextLong();	  
       			Mulcan[i]=Candies[i]*ballons[i];
 			MulcanSorted[i]=Candies[i]*ballons[i];
     		}		
@@ -24,25 +24,22 @@ public class Main{
 		Arrays.sort(MulcanSorted);
 		long start=MulcanSorted[0];
 		long end=MulcanSorted[n-1];
-		long result=0;
+		long result = end;
 		while(end>start)
 		{
             
 			long toCheck=(start+end)/2;
-            
 			int flag=0;
 			long m2=0;
 			for(int i=0;i<n;i++)
 			{
 				if(Mulcan[i]>toCheck)
 				{
-					m2+=Math.ceil((Mulcan[i]-toCheck+0.00)/Candies[i]);
-                   
+					m2+=(long)Math.ceil((Mulcan[i]-toCheck+0.00)/Candies[i]);
 				}
 				if(m2>m)
 				{
 					flag=1;
-                   
 					break;
 				}
 				
