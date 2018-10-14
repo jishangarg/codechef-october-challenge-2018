4f551b4af6c77a1fbf4dfb1baeb133f18a6ed82a
JISHAN GARGACHARYA <34843832+jishangarg@users.noreply.github.com>
2018-10-14 15:53:18 +0530

    Update Appy_and_Balloons.java

--- Appy_and_Balloons.java
+++ Appy_and_Balloons.java
@@ -2,23 +2,25 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args)
 	{
-	  Scanner in=new Scanner(System.in);
-    int n=in.nextInt();
-    int m=in.nextInt();
-    int[] ballons=new int[n];
-    int[] Candies=new int[n];
-    int[] Mulcan=new int[n];
-    int[] MulcanSorted=new int[n];		
-    for(int i=0;i<n;i++)
-    {
-      ballons[i]=in.nextInt();	    
-    }
+		Scanner in=new Scanner(System.in);
+    	  	int n=in.nextInt();
+    	  	int m=in.nextInt();
+    	  	int[] ballons=new int[n];
+    	  	int[] Candies=new int[n];
+    		int[] Mulcan=new int[n];
+    		int[] MulcanSorted=new int[n];		
+    		for(int i=0;i<n;i++)
+    		{
+      			ballons[i]=in.nextInt();	    
+    		}
 		
-    for(int i=0;i<n;i++)
-    {
-      Candies[i]=in.nextInt();	    
-    }		
-		
+    		for(int i=0;i<n;i++)
+    		{
+      			Candies[i]=in.nextInt();	  
+      			Mulcan[i]=Candies[i]*ballons[i];	    
+    		}		
+    
+    		
     		
 	}
 }
